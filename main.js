b0bf5087de688eecd818a37d6c9f0d467f21f0cc
akbarfarooq <akbarfarooq175@gmail.com>
2024-03-25 23:41:29 +0500

    this

---
+++ main.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1); //this
+let answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "myNumber",
+        message: "Please Guess the number between 1 to 6 ?",
+    },
+]);
+if (answer.myNumber === randomNumber) {
+    console.log("Congratulation!! You Guess Write Number");
+}
+else if (answer.myNumber > 6) {
+    console.log("Sorry!! Please select number between 1 to 6");
+}
+else {
+    console.log("Wrong Number");
+}
